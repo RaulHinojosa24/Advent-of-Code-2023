c9d1b35ae1d4f1041dc5ac6076acddff7dd8e332
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-01-13 11:03:21 +0100

    day 03 second part completed

--- Day03-GearRatios.js
+++ Day03-GearRatios.js
@@ -1,3 +1,63 @@
+// --- Day 3: Gear Ratios ---
+// You and the Elf eventually reach a gondola lift station; he says the gondola lift will take you up to the water source, but this is as far as he can bring you. You go inside.
+
+// It doesn't take long to find the gondolas, but there seems to be a problem: they're not moving.
+
+// "Aaah!"
+
+// You turn around to see a slightly-greasy Elf with a wrench and a look of surprise. "Sorry, I wasn't expecting anyone! The gondola lift isn't working right now; it'll still be a while before I can fix it." You offer to help.
+
+// The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+
+// The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
+
+// Here is an example engine schematic:
+
+// 467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..
+// In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
+
+// Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+
+// --> Your puzzle answer was 537732. <--
+
+// --- Part Two ---
+// The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+// You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+// Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+// The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+// This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+// Consider the same engine schematic again:
+
+// 467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..
+// In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+
+// What is the sum of all of the gear ratios in your engine schematic?
+
+// --> Your puzzle answer was 84883664. <--
+
 const DATA = `.......358..........31.....339.....669.............598......328.....575......................447..650..............964...........692........
 ...............415..*.........@......*...627*...................945*.............144/.506............................*......514...*...150...
 .........182..+.....873.756.......737........784..568....667..............258........./.........741...........707*....84........520.........
@@ -139,18 +199,6 @@ const DATA = `.......358..........31.....339.....669.............598......328...
 .............*........36..........743.=.../...............*......*..424.................580.#...897.448....*.......833...633.....*...*......
 .............963......................542........734.....901...914..........843.............523..........818..................691.....833...`
 
-const TEST_DATA = 
-`467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..`
-
 function checkIfPartNumber(x, y, data) {
   if (
     (!/\d|\./.test(data[y - 1]?.[x - 1]) && data[y - 1]?.[x - 1] !== undefined) ||
@@ -167,7 +215,6 @@ function checkIfPartNumber(x, y, data) {
   
   return false;
 }
-
 function gearRatios(data) {
   const splittedData = data.split('\n')
 
@@ -205,5 +252,93 @@ function gearRatios(data) {
   .reduce((acc, curr) => acc + curr)
 }
 
-const result = gearRatios(DATA)
+function getGears(x, y, data) {
+  const gears = []
+
+  if (data[y - 1]?.[x - 1] === "*") gears.push(`${x - 1}${y - 1}`)
+  if (data[y - 1]?.[x] === "*") gears.push(`${x}${y - 1}`)
+  if (data[y - 1]?.[x + 1] === "*") gears.push(`${x + 1}${y - 1}`)
+  if (data[y]?.[x - 1] === "*") gears.push(`${x - 1}${y}`)
+  if (data[y]?.[x + 1] === "*") gears.push(`${x + 1}${y}`)
+  if (data[y + 1]?.[x - 1] === "*") gears.push(`${x - 1}${y + 1}`)
+  if (data[y + 1]?.[x] === "*") gears.push(`${x}${y + 1}`)
+  if (data[y + 1]?.[x + 1] === "*") gears.push(`${x + 1}${y + 1}`)
+  
+  return gears;
+}
+
+function uniqueItems(array) {
+  return [...new Set(array)]
+}
+
+
+function gearRatios2(data) {
+  const splittedData = data.split('\n')
+
+  const numbers = []
+
+  let number = ""
+  let gears = [];
+
+  for (let y = 0; y < splittedData.length; y++) {
+    for (let x = 0; x < splittedData[y].length; x++) {
+      const current = splittedData[y][x]
+      
+      if (/\D/.test(current)) {
+        if (gears.length) {
+          numbers.push({
+            number: +number,
+            gears: uniqueItems(gears)
+          })
+        }
+        number = ""
+        gears = []
+
+        continue
+      } 
+
+      const newGears = getGears(x, y, splittedData)
+      number += current
+      gears = gears.concat(newGears)
+    }
+    if (number) {
+      numbers.push({
+        number: +number,
+        gears: uniqueItems(gears)
+      })
+    }
+    number = ""
+    gears = [];
+  }
+
+  const reduced = {}
+
+  for (let i = 0; i < numbers.length; i++) {
+    const curr = numbers[i]
+    for (let j = 0; j < curr.gears.length; j++) {
+      const gear = curr.gears[j]
+      if (reduced[gear]) {
+        reduced[gear].numbers.push(curr.number)
+      } else {
+        reduced[gear] = {
+          numbers: [curr.number]
+        }
+      }
+    }
+  }
+
+  let response = 0
+
+  for (const key in reduced) {
+    if (Object.hasOwnProperty.call(reduced, key)) {
+      const numbers = reduced[key].numbers;
+      
+      if (numbers.length === 2) response += numbers[0] * numbers[1]
+    }
+  }
+
+  return response
+}
+
+const result = gearRatios2(DATA)
 console.log(result);
