6641880ee5e8def9888f7339fae9a1e678dc66d0
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-01-17 14:14:51 +0100

    day 14 - patr 1 & 2 done

---
+++ Day14-Parabolic_Reflector_Dish.js
@@ -0,0 +1,288 @@
+const DATA = `....#..OO.O..OO..O...#....OO....#........#..#..OOO..#.....#..#.O#O.#...#......#..O......O....O#.#O.#
+.O.#.#.O.O.OO...O..O...O.O.OO.....O.....#...#..O#O....OO##.O...O..#........O......O.O#.....O##O.#.O#
+O.O#.....#.O#OOO..#O..#OO....###.#...#.O#....O..O...O###.......O...........O...OO.........O.....#.#.
+.O.O...O#O##.#.O.#..OOOOO##......##O....O.........#.......#.#.##.O.......O.......#...O.O....#..O....
+......#..#.##.OO.##..#..#OO.O#.#.O..........O.......#..O.O.O........O...O...##.O.O.#O........O.#OO..
+......##.O.....#.O..O##.OO.##O#..O.OO......O.OO..O..O#.....O..O.....O#O...........#.O.##..OO..O....O
+.....#.....O.O.#.##.....#.#..#.#.O#..##O#....#O..#.......#.O#OOOO...O...OO..O..O..O.#O.#..O.O..O.##.
+......#......O##.#..#..O#............OOO............O.......O...O..OO.....#OOO.#O#....#...O........#
+....#........O..O..O.##...O...#O...#.O.........O.O...#.OO#OO..OO.OO.....O#........#.#O..O.#........#
+..O#.O#....O......O#.#.O#..#...#..O#....OO....O...OO.O..O......O.....O...O##OO........#...OO#..##.O.
+.O#.O#..#..#O..O..O.##.O.O#.O.OOO.O.#O....O......OOO.#.....O.........#..#..O.....O..OO.#......OO..#.
+...OOO.#OO#....#.O.#..O...O....#....O...O..O.OO#.O...O....O#.#.O...........#...#.O...#...OO#.O......
+.O.#.O........#.....O..#.O#...O.#....#...O.......O.OO.OO#O..#..O..#.O...#.#....OO..O.O#..#.O.OO##...
+.O...OO..#.#.OO#O....O.O.....OO...#O......OO........O.O......#.#.#O#...O..OOO#....##..O.##....#....#
+...O...OO.O.#O.O..##.O.OO...#..O....O..O#.....#O.OO.......O.#........OO.........#..O.#...O#.O.......
+O..#.O....O..#..............O.....##.....O.#.....#...........O.#.O...O.....##....O..O..#.O.#O##..O..
+...O...O..O...O#..O#.OO#..O........#....#.O........#....#.....O...O.OO..O....#.##...O..#.O##..O..O..
+..OO...O...O..O#.O#O...#.##.#...#..OO.O.#.#..O....O..#O.O.....O...#.#.......O...####.....#.......#..
+.#O...O.O.......OO##...#.......#O.#..#OO...O......O...O#..#.....O#...O..O...........OOO.O...........
+#....O.O.O........O.O#...#....O.O.........#..#.#.O.O..O.O......#.O.OOO.O...#O..OO..O#...O....#.O..#.
+O.......#..O.......O..O..#.#O.O.....O#....O....O.O#..OO.#...O.#..O...#.O#...#..#.O.......#...O.###..
+.OO#...##.O.OO#..#O#O...O..#.O.O....OOO#.O.#O.OO.#O...O...##O..##....OO#...O.#.........##..#.O....#.
+......#...O.#...#O....O..O...#O.O....#O....#OOO.#....O.O.##..O..O....O........O.....#....O##.....O..
+O.O...O#.#O......#O...#......O..O..#....#...#.O#.O.O..#......#....#......O.#.OO..#..O...O.....#O#.OO
+#...O....O......O...#....#O.O..O.#......O#..##..#O#.#..O...O#..O..O##.OOO...#.OO..OO.#.#..##O.OO....
+O.....O...#........#.O....OOOO....O............#..O...O....#..O.....#..O....O...#.......OO....O.#O..
+#.O...#....#....#...#...OO...#O#..##..O...#.....OO....O..##...#..........#.....##OO.#...#.O.OO.#..O.
+#O#O#.###.......OOO.#....O....##.O...#.#....O...#.O#.......#...#O..O.O..O#..#...O#.#O..OO...#OO.#.O#
+.#O...O.........O...O.#O#O#.O.......##..##..##.O.#.#.O..OO.#.OO...OOO..#.O...O...#O..O.O#..#O#.OO..#
+.#..#O...O..O..OO#.#.O..##.......#O.####..#O...OO#O...O........O...OO...OO..O......#..O.......O#.O..
+.....#O#.#....OO.O.....O...###O...#.#........#...O..O....O##O#...###....O......O...O#..#..O...O.#.##
+..O...#..#..O..#.OO..O......O#OO....O...O..#..........##..O#.....#.....#.OO#.#O...O#..O#...#.#.##...
+..O...#...O###......O.O..O.#.O#..........O.......#O...#.##..O....O.O...#.#....O..O.#...#.O.#O.......
+..#.#..#O#.....#......O...##OO.O...#..#O#O..##.O....O##....OO....#....#..O.#........OO..##.O.....OO.
+O..#O.#..#.#...O.OO...OO...#.O#.#.O.O..#.........#....O...#.O..O#.....O......O...##.......#.##OO.O..
+..O.OO..O..#.....#.O#...#.O..OO..OO...OO..O..#O#........O.#.O.#.O...O#O........O.....O#..#.....O...#
+..O...OO.#.#O.OO.O##.##.O.O#O#.OO......OOOO.#........#.....O...#...#.##..#........#.OO..#.O.........
+..##.#OO#..O#.#.#....OO.O..............O..#....#.#.OO.O..O..OO..O.#..O.....O....#.....##...O.#O..O..
+..O.##..#..O...O....O..#........O.#..#.#.O......OO#OO.OO..OO.O###..#....O#.....#O..#....#OO#...#..O.
+.O....O.....#O.#.O...O..#.#....###......O...OO###.O....#.#O..O#..##OO...#..O.........O#...O.....O...
+O.OOO.O.#OO..#.......OOO..OO.#........O.O...OO......O....O#.O.O.....O......##....#......O....O...#..
+..##..#...O....O..#.#..#OO..O..............O.O....#....#OO...#.#.OO##O...OO.O#O....##.OO....#..##.#O
+...O....O#.#OO.O...#.O.##...#..O.#..#O.OO#.....O.O..O.....#....O#O....#.O.O..OO...O#O.O......###.O..
+...O.O.#.............O.....##OOOO....O...O...#..O......#.##.#O...O#....#..O##.O....#O.O..#OOO....#..
+.##OO..O...O.O.O....#.......O.#..#...OO...#..#..O.#.O.......O....O.....O.O.#.OO#O...#OO.O....#.#O...
+...O.#OOO#.O.#...OOO......O..#.##OOO......O..OO.##.##....##.#..OO....O..O..............#...OOO......
+........#..OOO.#.#...O.#..##.O#.O##.O.##.##.#..O#O.O#......#..#O.#....OO............#...###.OOO....#
+#.O.O.....O...........OO...#OOO#..###...#.O#.#.....O..#..#.#.OO......#....O#.....#.O....#O#.OO.O..O.
+O...........#..OO...OO...O..O.O##.....#....O##O.O.....O#..OO#.#O...#...OO..OO.......#O.#.#OO..#O###.
+O...O.OO........#...##OO.........O.#....O.......#....#.#...#..#O.#...#..............#...#........O..
+.#O....O.O....#...O##.#..O..OO#..#............#O...........O.OO#..##......O...O..#........#..#......
+...............#.........#O..#....O..##.O......#....#.OO.#.#.O.O.OO....O.....OOO...........O.#..O..O
+......O.#O..........O#O.#..O#.O#.......#.O...O...#.....O.....#..O...#..#..O...##O#..O#.O...#.OOO..O.
+##O#...#..O#........#O.....O...#.#OO#O...O.##..OO..O....OO.O.#...O......O..#...#.O.#..##OO....O..O.#
+........O......#.OO..OO.OOO.O...O.OO.....O..#..OO#O.O....O......O.#..OO..O#O..#.O#..#....O..O..#.#..
+##..OOO.#OOO..O.O.O..O.#..O..O#..OOO.#..OO..OO..#.#.OOO...##..O#..O.#..#.#O....O..........#....O..OO
+.......#.....O......O#O...##.....###...#O...#....#.#....O....O..O.O..O...#OO...#O..OO.....O.#..O....
+O.#...O..#.....O..#..O....O#.....#..#.#..###....O.OO...#O..#O#.##..O.#...O.##O....OO.O...O#.O..O..#.
+....#.O..#O.....O..OO#OOO#....#...#.#..#.#.....O....O..O....#....O#....O........O..O#.#.#.###O.....O
+.....#O...#.#....O.....###...O....#.#...O#..O..##...O.O....#..O#.#...O.OO..#OO.....O...#..OOO....O..
+#O...O.........#...#.OO....O.....#....##.#..#..##O.OO#O...O#O.....#..O...O...###.O#....#OO.##O.#O...
+O..OOO..#O.O.O#..O..O.O.##..O..OO#O....O....#..OO#.OO.#.O...O.....O.O..##...#....#...O....O........O
+...OO.O.....#..#.#....O...OO...#O......O.#..O#..O..O.......O.O....#.O....O.##.....OO#..#..O#.OO#O...
+..........OO....#..#O.O..#....O#.#.....#...#...OO...#..O.###O...##.##..#.....#.#..O.O..O...###O.....
+O..#...OO..O#....O...#O.....O....#....#..#O...#.O#..#..O...O#O.#O....O#O....O#..O.#.#.#..O.....O...O
+.O.O...O.......OO..O...#......OO.#.#.#....#..#....#O.........#OO..O...#..O..O#.#..O..##..O.#O..#..O.
+.#.O..O#..O..O#O.O...O#.....O..O..#..#...OO#...........#.O.OO...#..O.#.....#........#O.#.O....#O.O..
+.O.....O..O...O..O......O#.O..O...........##..O.#.O.O.O.OOO....#.O..#O.#.##.......O#.#.....##...#O..
+.O.O....#..#.#O....O.#......O.#.O#......O##..O....#.#...#.#.OO.......#OO#.##OO.....O.#O....OO.......
+#O#....OO...O......O....OO#O.....O.#.O.....#.O......O..OO..O..OOO.O..#.O#..#..#.....O.O..#.O...#O.##
+.......O.#O..#.#.....#.O.#..O..OOOO.#.O.O#...##.#...O.#OO...##.....#..O.O...#.....O.......#O.O#O...#
+#O..#.#..OO.O.O...#.........#O.O#.......O#..#O.O##.#...O.##O.O..#.O......OOO.O....#...O.O...#......#
+..O##O.O#.O...O.......O..#...##..O.#.O.OO.OO#.....O.#..............O.O#.##.O.O....#..#...O.....#..##
+..............O.........#.##......#....##...#...O.O##O.O..##..O.....O#....O.#.....O..##..#..OO....##
+#..#......O.O.O..O..O...#.#.#.OO..O#.O..O##OO#...#O...O.O.................#O.....#O.O#O....#......OO
+..O#O#......O..#O......#.........O#O.#.O.....O.#.....O..O..O.##....#....OO....##..O.O.....O...O...O#
+..O.O...O....OO......O....O..O.#O.O........OO......O#...OO##.........#.#...#.#......##O.##O....O.O#.
+..O...#OO...O........O.O.....#.O..###O..#.....O.......#.....O...#O.#.....#....##..#....O.##.........
+..#.#O.......O#..........#.#..#...O.....O.OO#..O...O.#....OO.O.OO....O..O.O#.....#..O.....#O..O..#O#
+OO..O##..O.O...#.O..O..#....O..O....O..##.#..#O.O.......#.....O#..O.O.OOO..OOO#O##.#..#O.......#....
+...O#.O..#.....OO.#O.O#O.O.O##..OO..O......O..##.....O....#.##....O..#.O....OO....O.#OO..O...#..O#..
+O...OOO...#.O#..O#.O.##.#O.O..OOO..O...OO#OOO...O....#..O#...OO#.O..#.....O#.O...#..........OO#.#.##
+...OO.#O#....O#...#.#......O....O#........O#...#O#...#...O.....#.OO#...##.OO.OO...#..O.O.....O..O...
+.##..O..#O#O...#O.O..O#....#..#.......#....#.##.......#...##.O#OO......O#O.....O#.O.##...O.....OOOOO
+.OO.#.O#....OO.#OO.#O.O.......O.#......#.#O#.....O......##..O#O#O...#....O.O...#...O#....O...#.O....
+.O##.O..O...O.....O#......OO.O..##...........##..#O...#...O#O.O...O#...###..#.O.O#O.#O..#......#.OO.
+O.OO#..O..OO.O...#OO.#.OO.#...#.#...O...O..O#OOO..OO..O......O............O.....O#..O...#OO.O.#O#OO#
+O##O......#..#...#.##O#.O.......#O#.O.............O.......#..O...#.OOO..O..O..OO#O#.O##..#O.#....OO#
+.....OO#.O..O..OOO.O..OO.........O#...#.....O.....#..#..O....#O#.O....O....OO..#...OO.O.O#...#.O....
+...#....#......#...O#O....O##.OO..#.O#OO#.OO..#.##..OO..#..O#..O......O......OO#..O.O..........O....
+...O...#...OOOOO......O...#...#.............O#O..OO...O.#...O.O..#O.OO..O.O.O...O.......#.....O.##..
+....O.#...O..#...#...O.......#....#..O.O..#.#...#..O.....O#O.....O...O.........#OO.O.#...#.......O..
+.#.O...O.#.#O.O.#..##..#O....#OO...OO.#..O#O.OO....#O#OO...#.O......#....#...O....#.....O..#.....##.
+..#.O.OOO#O#.OO.O....#O.........O.OO.#.#.#...O#O........O..#...O....O.O#.O...#....#.OOO.#..##...#.O.
+....#O#O#O#.#.#.#O.....#..O..##O........##.O.......O.O#.##O..O..O......#OO.O.#.OOO...O..#..O.....O..
+#.O...O.#.#OO..OO.O.....O...OOO....##.O...#O....O...O.#O.##.....O#...O.OO.#.....O....O.O..O.O......O
+..OO#......O.....O......O...#.........#.OO....OO......O...#....#OOO...O.O.OO.....#..O...OO....OOO#O#
+.O...#.#.O...#..........#.O..O#.....O..##..O....O..#..#....O..O.O.OO#.O...O.#.O....#........###..OO.
+....O.#....O...O.O..#OO##......#.#O.........OO......O#.O.#..OOO.#...OO...#O##.#.O......#...O...O....
+#.O..O..#..O#.#....O...#...O#O#O...OO.OO...##....#..OO#...#...O.#.#O#..O..........#O.O..O...OOO.#...`
+
+const TEST_DATA = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+
+function findSlideSpot(x, y, direction, map) {
+  if (direction === "N") {
+    for (let spot = y - 1; spot >= 0; spot--) {
+      const el = map[spot][x]
+  
+      if (["#", "O"].includes(el)) return spot + 1
+    }
+    return 0
+  }
+  if (direction === "W") {
+    for (let spot = x - 1; spot >= 0; spot--) {
+      const el = map[y][spot]
+  
+      if (["#", "O"].includes(el)) return spot + 1
+    }
+    return 0
+  }
+  if (direction === "S") {
+    for (let spot = y + 1; spot < map.length; spot++) {
+      const el = map[spot][x]
+  
+      if (["#", "O"].includes(el)) return spot - 1
+    }
+    return map.length - 1
+  }
+  if (direction === "E") {
+    for (let spot = x + 1; spot < map[0].length; spot++) {
+      const el = map[y][spot]
+  
+      if (["#", "O"].includes(el)) return spot - 1
+    }
+    return map[0].length - 1
+  }
+}
+
+function slideRocksNorth(map) {
+  for (let y = 1; y < map.length; y++) {
+    const row = map[y];
+    for (let x = 0; x < row.length; x++) {
+      const spot = row[x];
+      if (spot === "O") {
+        const slideSpot = findSlideSpot(x, y, "N", map)
+        map[y][x] = "."
+        map[slideSpot][x] = "O"
+      }
+    }
+  }
+}
+function slideRocksWest(map) {
+  for (let x = 1; x < map[0].length; x++) {
+    for (let y = 0; y < map.length; y++) {
+      const spot = map[y][x];
+      if (spot === "O") {
+        const slideSpot = findSlideSpot(x, y, "W", map)
+        map[y][x] = "."
+        map[y][slideSpot] = "O"
+      }
+    }
+  }
+}
+function slideRocksSouth(map) {
+  for (let y = map.length - 2; y >= 0; y--) {
+    const row = map[y];
+    for (let x = 0; x < row.length; x++) {
+      const spot = row[x];
+      if (spot === "O") {
+        const slideSpot = findSlideSpot(x, y, "S", map)
+        map[y][x] = "."
+        map[slideSpot][x] = "O"
+      }
+    }
+  }
+}
+function slideRocksEast(map) {
+  for (let x = map[0].length - 2; x >= 0 ; x--) {
+    for (let y = 0; y < map.length; y++) {
+      const spot = map[y][x];
+      if (spot === "O") {
+        const slideSpot = findSlideSpot(x, y, "E", map)
+        map[y][x] = "."
+        map[y][slideSpot] = "O"
+      }
+    }
+  }
+}
+
+function calculateLoad(map) {
+  let load = 0
+  for (let i = 0; i < map.length; i++) {
+    const row = map[i];
+    const rowCoef = map.length - i
+
+    load += rowCoef * row.filter(c => c === "O").length
+  }
+  return load
+}
+
+function displayMap(map) {
+  for (let index = 0; index < map.length; index++) {
+    const row = map[index];
+    console.log(row.join(""));
+  }
+}
+
+function alreadyBeenThere(helper, map2) {
+  if (!helper.length) return -1;
+
+  for (let a = 0; a < helper.length; a++) {
+    const map1 = helper[a]
+    let same = true;
+    
+    for (let i = 0; i < map1.length && same; i++) {
+      const row1 = map1[i];
+      const row2 = map2[i];
+      for (let j = 0; j < row1.length && same; j++) {
+        const el1 = row1[j];
+        const el2 = row2[j];
+        if (el1 !== el2) same = false
+      }
+    }
+
+    if (same) {
+      return a
+    }
+  }
+  
+  return -1
+}
+
+function parabolicReflectorDish(data) {
+  const map = data.split("\n").map(r => r.split(""))
+  
+  slideRocksNorth(map)
+  
+  const load = calculateLoad(map)
+
+  return load
+}
+
+function parabolicReflectorDishPart2(data) {
+  const map = data.split("\n").map(r => r.split(""))
+  let helper = [];
+  let cycleStart, cycleLength
+  
+  for (let i = 0; i < Infinity; i++) {
+    const alrIndex = alreadyBeenThere(helper, map)
+    if (alrIndex !== -1) {
+      cycleLength = i - alrIndex
+      cycleStart = alrIndex
+      break
+    }
+
+    helper.push(structuredClone(map))
+
+    slideRocksNorth(map)
+    slideRocksWest(map)
+    slideRocksSouth(map)
+    slideRocksEast(map)
+  }
+
+  const lastCycles = (1000000000 - cycleStart) % cycleLength
+  
+  for (let i = 0; i < lastCycles; i++) {
+    slideRocksNorth(map)
+    slideRocksWest(map)
+    slideRocksSouth(map)
+    slideRocksEast(map)
+  }
+  
+  const load = calculateLoad(map)
+
+  return load
+}
+
+const response = parabolicReflectorDishPart2(DATA)
+console.log(response);
